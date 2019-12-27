0a0286e2cafd5736ce3ee7d99726b8037cafbf91
jasl <jasl9187@hotmail.com>
2019-12-28 00:06:19 +0800

    port to sgx

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,12 @@
 //! Hex binary-to-text encoding
 
 #![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(all(feature = "mesalock_sgx", not(target_env = "sgx")), no_std)]
+#![cfg_attr(all(target_env = "sgx", target_vendor = "mesalock"), feature(rustc_private))]
+
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
 
 #[cfg(feature = "std")]
 extern crate core;
